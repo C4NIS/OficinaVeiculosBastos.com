eaffb8c0c3d45fe4bd187a421e05907328864a92
Isaac <mathebastos@hotmail.com>
2023-10-23 09:38:30 -0300

    Imagem e texto principal alinhados, botão criado

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -4,10 +4,11 @@ const Header = () =>{
     return(
         <nav className = "container">
             
-            <a href="https://www.youtube.com/watch?v=ULQhvIGG27Q&ab_channel=LofiGirl" alt='Para pagina principal'>
-                <img  src = "/assets/icon.svg" alt='logo' />
-            </a>
-            
+            <div className="container_logo">
+                <a href="https://www.youtube.com/watch?v=ULQhvIGG27Q&ab_channel=LofiGirl" alt='Para pagina principal'>
+                    <img src = "/assets/icon.svg" alt='logo' />
+                </a>
+            </div>
 
             <div className="container_itens">
                 <a href="https://www.youtube.com/watch?v=ULQhvIGG27Q&ab_channel=LofiGirl" alt='Página Principal'>Página Principal</a>
--- frontend/src/components/principal/index.js
+++ frontend/src/components/principal/index.js
@@ -1,3 +1,4 @@
+import Button from "../button";
 import "./style.css";
 
 const Principal = () => {
@@ -10,8 +11,10 @@ const Principal = () => {
             <div className="container_textos_principais">
                 <h2>Bem-vindo à Bastos Mecânica </h2>
                 <p>Somos a escolha confiável para todas as suas necessidades de manutenção e reparo de veículos empresariais em São José dos Campos. Com anos de experiência no setor automobilístico e um compromisso inabalável com a excelência, a Bastos é a sua parceira deconfiança quando se trata de manter sua frota empresarial funcionando perfeitamente.</p>
+                <div className="container_botao_principal">
+                    <Button>Agende sua visita</Button>
+                </div>
             </div>
-
         </div>
     );
 };
