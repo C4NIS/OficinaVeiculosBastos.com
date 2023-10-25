5132539d9fee40b17632845bc4189c51994ad26c
Isaac <mathebastos@hotmail.com>
2023-10-25 09:25:35 -0300

    Finalizado

--- frontend/src/components/module/module_entry/index.js
+++ frontend/src/components/module/module_entry/index.js
@@ -11,11 +11,11 @@ const Entry = () => {
 
             <h2 className="title_entry">Bastos Mecânica: O Seu Parceiro de Confiabilidade Automotiva!</h2>
 
-            <p>Somos a escolha confiável para todas as suas necessidades de manutenção e reparo de veículos empresariais em São José dos Campos. Com anos de experiência no setor automobilístico e um compromisso inabalável com a excelência, a Bastos é a sua parceira deconfiança quando se trata de manter sua frota empresarial funcionando perfeitamente.</p>
+            <p className="paragraph_entry">Somos a escolha confiável para todas as suas necessidades de manutenção e reparo de veículos empresariais em São José dos Campos. Com anos de experiência no setor automobilístico e um compromisso inabalável com a excelência, a Bastos é a sua parceira deconfiança quando se trata de manter sua frota empresarial funcionando perfeitamente.</p>
 
             <div className="container_button_entry">
 
-                <Button>Agendar sua visita</Button>
+                <Button>Agendar visita</Button>
 
             </div>
 
